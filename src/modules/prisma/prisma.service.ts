d23ca0b24ba74b8b0c36c0d5503f88b26de974db
FuHang <84437153@qq.com>
2023-04-12 22:48:12 +0800

    fix: 修改prisma模块信息

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -2,8 +2,8 @@
  * @Description:
  * @Author: FuHang
  * @Date: 2023-04-12 17:31:55
- * @LastEditTime: 2023-04-12 17:33:22
- * @LastEditors:
+ * @LastEditTime: 2023-04-12 20:14:19
+ * @LastEditors: Please set LastEditors
  * @FilePath: \nest-service\src\modules\prisma\prisma.service.ts
  */
 import { INestApplication, Injectable } from '@nestjs/common';
@@ -11,6 +11,10 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
