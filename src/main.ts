53c8afb30b6a673fd8e04bcc66c34bda87044906
fuhang0512 <84437153@qq.com>
2023-04-12 17:40:15 +0800

    feat: 添加swagger相关配置

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: FuHang
  * @Date: 2023-04-12 00:23:41
- * @LastEditTime: 2023-04-12 00:27:13
+ * @LastEditTime: 2023-04-12 17:38:30
  * @LastEditors: Please set LastEditors
  * @FilePath: \nest-service\src\main.ts
  */
@@ -12,13 +12,25 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
 
-  await app.listen(3000);
+  // 设置swagger文档相关配置
+  const config = new DocumentBuilder()
+    .setTitle('Naive Admin 后台管理系统接口文档')
+    .setDescription('后台管理系统接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
