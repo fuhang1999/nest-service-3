8e7c33f3f9e62eae2c6462be94473876e196ff7d
fuhang0512 <84437153@qq.com>
2023-04-12 17:58:20 +0800

    feat: 添加用户模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@
  */
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
