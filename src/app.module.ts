760903db88eafe57f12096505ead2c50f4ed8e11
fuhang0512 <84437153@qq.com>
2023-04-12 18:03:08 +0800

    feat: 添加部门模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,10 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { RoleModule } from './modules/role/role.module';
 import { MenuModule } from './modules/menu/menu.module';
+import { DepartmentModule } from './modules/department/department.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, RoleModule, MenuModule],
+  imports: [PrismaModule, UserModule, RoleModule, MenuModule, DepartmentModule],
   controllers: [],
   providers: [],
 })
