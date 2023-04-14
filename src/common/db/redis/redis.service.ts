d521305a345a18a91672a42b6919ca73b31fb23a
fuhang0512 <84437153@qq.com>
2023-04-14 19:25:52 +0800

    feat: 添加redis模块

---
+++ src/common/db/redis/redis.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+const redisList = []; // 用于存储 redis 实例
+const redisIndex = []; // 用于记录 redis 实例索引
+
+@Injectable()
+export class RedisService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async initRedis(method: string, db = 0) {
+    const redisOption = {
+      host: this.configService.get('REDIS_HOST'),
+      port: Number(this.configService.get('REDIS_PORT')),
+      //   password: this.configService.get('REDIS_PASSWORD'),
+    };
+
+    const isExist = redisIndex.some((x) => x === db);
+    if (!isExist) {
+      // Logger.debug(`[Redis ${db}]来自 ${method} 方法调用 `);
+      redisList[db] = new Redis(Object.assign(redisOption, { db }));
+      // redisList[db] = new Redis({ ...redisOption, db });
+      redisIndex.push(db);
+    } else {
+      // Logger.debug(`[Redis ${db}]来自 ${method} 方法调用`);
+    }
+    return redisList[db];
+  }
+
+  async setRedis(
+    method: string,
+    db = 0,
+    key: string,
+    val: string,
+    timeout = 60 * 60,
+  ) {
+    if (typeof val == 'object') {
+      val = JSON.stringify(val);
+    }
+    const redis = await this.initRedis(method, db);
+    redis.set(`${key}`, val);
+    redis.expire(`${key}`, timeout);
+  }
+
+  async getRedis(method: string, db = 0, key: string) {
+    return new Promise(async (resolve, reject) => {
+      const redis = await this.initRedis(method, db);
+      redis.get(`${key}`, (err, val) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(val);
+      });
+    });
+  }
+}
